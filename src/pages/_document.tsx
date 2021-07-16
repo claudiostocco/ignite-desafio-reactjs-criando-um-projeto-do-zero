6162e4bfdfd9d54aaeaa96ab2f0dfaa046eb3fe8
claudiostocco <claudiostocco@gmail.com>
2021-07-16 08:10:17 -0300

    Pagina de exibição do post concluida.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,16 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
+  render(): JSX.Element {
     return (
       <Html>
         <Head title="Spacetraveling">
-
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-
-          
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,15 +1,16 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
-import Prismic from '@prismicio/client'
+import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
-import { format } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../../services/prismic';
 import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { useRouter } from 'next/router';
 
 interface Post {
   first_publication_date: string | null;
@@ -32,82 +33,108 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ post }: PostProps) {
-  return (<>
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
+  function readingTime(): number {
+    const qtd = post.data.content.reduce((acc, contentValue) => {
+      const qtdHeading = contentValue.heading.split(/\s/g).length;
+
+      const qtdBody = contentValue.body.reduce((accBody, valueBody) => {
+        const bodyWords = valueBody.text.split(/\s/g).length;
+        return accBody + bodyWords;
+      }, 0);
+
+      return qtdHeading + qtdBody;
+    }, 0);
+    return Math.ceil(qtd / 200) + 1;
+  }
+  return (
+    <>
       <Header />
       <main className={styles.container}>
         <div className={styles.imageContainer}>
-          <img src="" alt="" />
+          <img src={post.data.banner.url} alt="" />
         </div>
-        <article className={styles.postContainer}>
+        <section className={styles.postContainer}>
           <header>
-            <h1>post.data.title</h1>
+            <h1>{post.data.title}</h1>
             <div className={styles.info}>
               <FiCalendar />
-              <time>{format(new Date(), "dd MMM yyyy", { locale: ptBR })}</time>
+              <time>
+                {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+                  locale: ptBR,
+                })}
+              </time>
               <FiUser />
-              <span>post.data.author</span>
+              <span>{post.data.author}</span>
               <FiClock />
-              <span>4 min</span>
+              <span>{readingTime()} min</span>
             </div>
           </header>
-          <section className={styles.postContent}>
-
-          </section>
-
-        </article>
+          {post.data.content.map(content => (
+            <article className={styles.postContent}>
+              <h2>{content.heading}</h2>
+              <div
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: RichText.asHtml(content.body),
+                }}
+              />
+            </article>
+          ))}
+        </section>
       </main>
     </>
-  )
+  );
 }
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
-  const posts = await prismic.query([
-    Prismic.predicates.at('document.type', 'posts')
-  ],{
-    fetch: [],
-    pageSize: 100
-  });
+  const posts = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: [],
+      pageSize: 100,
+    }
+  );
 
   return {
-    paths: posts.results.map(post => ({params: {slug: post.uid}})),
-    fallback: true
-  }
+    paths: posts.results.map(post => ({ params: { slug: post.uid } })),
+    fallback: true,
+  };
 };
 
-export const getStaticProps = async context => {
-  const {slug} = context.params;
-  console.log(slug);
+export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
 
-  const prismic = getPrismicClient(context.req);
-  const response = await prismic.getByUID('posts',String(slug),{});
+  // const postContent = {
+  //   first_publication_date: response.first_publication_date,
+  //   data: {
+  //     title: response.data.title,
+  //     banner: {
+  //       url: response.data.banner,
+  //     },
+  //     author: response.data.author,
+  //     content: response.data.content.map(content => ({
+  //       heading: content.heading,
+  //       body: content.body.map(body => ({
+  //         text: body,
+  //       })),
+  //     })),
+  //   },
+  // };
 
-  console.log(response.data.content[0].body)
-
-  const postContent = {
-    first_publication_date: response.first_publication_date,
-    data: {
-      title: response.data.title,
-      banner: {
-        url: response.data.banner
-      },
-      author: response.data.author,
-      content: response.data.content.map(content => ({
-        heading: RichText.asHtml(content.heading),
-        body: content.body.map(body => ({
-          text: RichText.asHtml(body.text)
-        }))
-      }))
-    }
-  };
-
-  console.log(postContent)
+  // console.log(postContent);
 
   return {
     props: {
-
+      post: response,
     },
-    revalidate: 30 * 60 // Minutes
-  }
+    revalidate: 30 * 60, // Minutes
+  };
 };
